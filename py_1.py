4ed4161ee411ed73dbb35d215adcfb022e204957
MINEMOI <minemoi197@gmail.com>
2019-01-15 16:31:18 -0600

    aprendiendo a programar con python, logre hacer el juego de adivinar el numero con 5 vidas

--- py_1.py
+++ py_1.py
@@ -1,2 +1,35 @@
 nombre=input("dime tu nombre")
-print("hola {},".format(nombre))
+print("hola {}, hoy vamos a jugar a un juego".format(nombre))
+respuesta=int(input("¿estas listo?"))
+if respuesta == 1:
+    print("ok {}, pues empecemos".format(nombre))
+    number_to_gess = 5
+    user_number = int(input("dime un numero del uno al 10"))
+    if user_number == number_to_gess:
+        print("ganaste")
+    else:
+        print("perdiste, intentalo de nuevo")
+    user_number = int(input("dime un numero del uno al 10"))
+    if user_number == number_to_gess:
+        print("ganaste")
+    else:
+        print("perdiste, intentalo de nuevo")
+    user_number = int(input("dime un numero del uno al 10"))
+    if user_number == number_to_gess:
+        print("ganaste")
+    else:
+        print("perdiste, intentalo de nuevo")
+    user_number = int(input("dime un numero del uno al 10"))
+    if user_number == number_to_gess:
+        print("ganaste")
+    else:
+        print("perdiste, intentalo de nuevo")
+    user_number = int(input("dime un numero del uno al 10"))
+    if user_number == number_to_gess:
+        print("ganaste")
+    else:
+        print("perdiste, gracias por participar")
+
+else:
+    print("pues vete a la chingada")
+print("adios")
