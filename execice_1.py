de8ee511e023e29508846f507aef84b22a2616c7
MINEMOI <minemoi197@gmail.com>
2019-01-16 17:28:03 -0600

    el ejercicio 2 de la clace 9 de nate gentile resuelto

--- execice_1.py
+++ execice_1.py
@@ -2,18 +2,13 @@ i = 0
 comas = 0
 space = 0
 points = 0
+mayusculas = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Ñ", "Z", "X", "C", "V", "B", "N", "M", ]
+capital_letter = 0
 
 user_input = input("cuentame tu vida")
 for i in user_input:
-    if i == (","):
-        comas += 1
-    if i == (" "):
-        space += 1
-    if i == ("."):
-        points += 1
+   if i in mayusculas:
+       capital_letter += 1
 
-print(f"las comas fueron: {comas}")
-print()
-print(f"los espacios fueron: {space}")
-print()
-print(f"los puntos fueron: {points}")
+print(f"Mayusculas: {capital_letter}")
+
